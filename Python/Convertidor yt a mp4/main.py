830cdbc2605ab1ecb852e083c4af874ac16d1b05
uo287577 <uo287577@uniovi.es>
2024-12-27 21:56:20 +0100

    Generador de contraseñas finalizado

--- Python/Convertidor yt a mp4/main.py
+++ Python/Convertidor yt a mp4/main.py
@@ -8,11 +8,8 @@ def descargar_video(url, ruta_descarga):
     except Exception as e:
         print("Ocurrió un error:", str(e))
 
-# Ejemplo de uso
 url = input("Introduce la URL del video de YouTube: ")
 ruta_descarga = 'D:\TRABAJOS\Desktop'
 descargar_video(url, ruta_descarga)
 
 # hacer en el escritorio una carpeta llamada 'Descargas'
-# en la terminal: pip install pytube
-# pyinstaller --onefile convertidor_youtube_mp4.py
--- Python/Generador de contraseñas/main.py
+++ Python/Generador de contraseñas/main.py
@@ -1,17 +1,56 @@
+from zxcvbn import zxcvbn
 import random as rd
+import string
 
-passwordGenerada = ""
-vocabulario = ''.join(chr(i) for i in range(0x110000) if chr(i).isprintable())
+catalogoSeguridad = {
+    0: "Débil",
+    1: "Insuficiente",
+    2: "Normal",
+    3: "Bueno",
+    4: "Máximo"
+}
 
-longitud = None
+def solicitarLongitud():
+    longitud = None
+    
+    while longitud == None:
+        try:
+            
+            longitud = int(input("Ingresa la longitud que quieras que tenga la contraseña: "))
+        except ValueError as e:
+            print("Tienes que ingresar un valor numérico para la longitud de la cadena")
 
-while longitud == None:
-    try:
-        longitud = int(input("Ingresa la longitud que quieras que tenga la contraseña: "))
-    except ValueError as e:
-        print("Tienes que ingresar un valor numérico para la longitud de la cadena")
+    return longitud
+    
+def generarPassword(longitud, vocabulario):
+    puntuacionObtenida = 0
 
-for i in range(longitud):
-    index = rd.randint()
-    print(index, "\n")
+    while puntuacionObtenida < 3: # si la seguridad de la contraseña generada no es la deseada -> volver a generar una nueva contraseña
+        passwordGenerada = ""
+
+        for i in range(longitud):
+            passwordGenerada += vocabulario[rd.randint(0, len(vocabulario)-1)]
+
+        try:
+            puntuacionObtenida = zxcvbn(passwordGenerada)["score"]
+        except IndexError: # Si ocurre un error (por ejemplo, en contraseñas muy simples)
+            continue
+
+    tiempoEstimadoParaSuFiltrado = zxcvbn(passwordGenerada)["crack_times_display"]["offline_slow_hashing_1e4_per_second"] # El tiempo estimado para adivinar la password generada
+    return passwordGenerada, catalogoSeguridad[puntuacionObtenida], tiempoEstimadoParaSuFiltrado
+
+
+
+def main():
+    vocabulario = [c for c in (string.ascii_letters + string.digits + string.punctuation)]
+
+    password, puntuacion, estimacionFiltrado = generarPassword(solicitarLongitud(), vocabulario)
+
+    print("\n\t==============================================================")
+    print("\tContraseña generada:", password)
+    print("\tNivel de seguridad logrado", puntuacion)
+    print("\tTiempo estimado para que logren adivinar la contraseña:", estimacionFiltrado)
+    print("\t==============================================================\n")
+
+main()
 
