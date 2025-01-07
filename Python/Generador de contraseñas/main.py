13b704f389d09d07bcfb2b5fdd27b840bc8dd49b
uo287577 <uo287577@uniovi.es>
2025-01-07 20:10:39 +0100

    Generador de contraseñas finalizado

--- Python/Generador de contraseñas/main.py
+++ Python/Generador de contraseñas/main.py
@@ -1,7 +1,10 @@
 from zxcvbn import zxcvbn
 import random as rd
 import string
+import asyncio
 
+vocabulario = [c for c in (string.ascii_letters + string.digits + string.punctuation)]
+longitudPassword = 16
 catalogoSeguridad = {
     0: "Débil",
     1: "Insuficiente",
@@ -10,25 +13,26 @@ catalogoSeguridad = {
     4: "Máximo"
 }
 
-def solicitarLongitud():
-    longitud = None
-    
-    while longitud == None:
-        try:
-            
-            longitud = int(input("Ingresa la longitud que quieras que tenga la contraseña: "))
-        except ValueError as e:
-            print("Tienes que ingresar un valor numérico para la longitud de la cadena")
+def imprimirPresentacion():
+    print("\t==============================================================")
+    print("\tGenerador de contraseñas seguras")
+    print("\tAutor: Miguel Fernández Huerta")
+    print("\t==============================================================")
 
-    return longitud
-    
-def generarPassword(longitud, vocabulario):
+async def confirmacionProceso():
+    entrada = str(input("\tPulse cualquier tecla para iniciar el proceso de generación de contraseña"))
+    if(entrada != None):
+        print("\n\tLa contraseña se está generando (este proceso puede llevar unos minutos u horas)")
+        password, puntuacion, estimacionFiltrado = await generarPassword(vocabulario)
+    imprimirResultados(password, estimacionFiltrado)
+
+async def generarPassword(vocabulario):
     puntuacionObtenida = 0
+    passwordGenerada = ""
 
-    while puntuacionObtenida < 3: # si la seguridad de la contraseña generada no es la deseada -> volver a generar una nueva contraseña
-        passwordGenerada = ""
+    while puntuacionObtenida != 4: # si la seguridad de la contraseña generada no es la máxima -> volver a generar una nueva contraseña
 
-        for i in range(longitud):
+        for i in range(longitudPassword):
             passwordGenerada += vocabulario[rd.randint(0, len(vocabulario)-1)]
 
         try:
@@ -39,18 +43,15 @@ def generarPassword(longitud, vocabulario):
     tiempoEstimadoParaSuFiltrado = zxcvbn(passwordGenerada)["crack_times_display"]["offline_slow_hashing_1e4_per_second"] # El tiempo estimado para adivinar la password generada
     return passwordGenerada, catalogoSeguridad[puntuacionObtenida], tiempoEstimadoParaSuFiltrado
 
-
-
-def main():
-    vocabulario = [c for c in (string.ascii_letters + string.digits + string.punctuation)]
-
-    password, puntuacion, estimacionFiltrado = generarPassword(solicitarLongitud(), vocabulario)
-
-    print("\n\t==============================================================")
+def imprimirResultados(password, estimacionFiltrado):
+    print("\t==============================================================")
     print("\tContraseña generada:", password)
-    print("\tNivel de seguridad logrado", puntuacion)
     print("\tTiempo estimado para que logren adivinar la contraseña:", estimacionFiltrado)
     print("\t==============================================================\n")
+
+def main():
+    imprimirPresentacion()
+    asyncio.run(confirmacionProceso())
 
 main()
 
